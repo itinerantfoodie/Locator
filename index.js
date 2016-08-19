cea8e6c68e82f6605b3c85c1b4e2673cdac22a81
Barry <barry.teoh@perceptionz.net>
2016-08-19 13:48:07 +0800

    Use a more resilient history function (if there is a lot of checkin history off the grid)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var uuid = require('node-uuid');
 var doc = require('dynamodb-doc');
 var dynamo = new doc.DynamoDB();
 var uuid = require('node-uuid');
+var foursquare = require('./lib/foursquare');
 
 exports.handler = function(event, context) {
   var dbparams = {
@@ -46,80 +47,64 @@ exports.handler = function(event, context) {
                 if (!err) {
                   if (res.Count == 0) {
                     // Lets grab some data if the record is old
-                    //var url = "https://api.foursquare.com/v2/users/self?oauth_token=" + oauth_token + "&v=20160226";
-                    var url = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=" + oauth_token + "&v=20160226";
-                    https.get(url, function(res) {
-                      var body = '';
-                      res.on('data', function(d) {
-                          body += d;
-                      });
-                      res.on('end', function() {
-                        try {
-                            var respObj = JSON.parse(body);
-                        } catch (e) {
-                            // Handle exceptions
-                            response["status"] = "An internal error has occured while parsing the response from the remote service - " + e.toString();
-                        }
-                        if (respObj.meta.code == 200) {
-                          response["status"] = "OK";
-                          var checkinObj = respObj.response.checkins;
-                          var checkinHistory = checkinObj.items;
-                          var lastCheckin = checkinHistory[0];
-                          var checkinIsVisible = true;
-                          if (lastCheckin.visibility != undefined) {
-                            checkinIsVisible = false;
-                            console.log("Checkin not visible");
-                          } else {
-                            console.log("Checkin is visible");
-                          }
-                          if (checkinIsVisible == false) {
-                            for (var i = 1; i < checkinHistory.length; i++) {
-                              var checkinHistoryItem = checkinHistory[i];
-                              // If there is no visibility set for this checkin its good assuming we haven't already found a non off the grid checkin
-                              if (checkinHistoryItem.visibility == undefined && checkinIsVisible == false) {
-                                // Set lastCheckin to the next item thats available
-                                lastCheckin = checkinHistoryItem;
-                                checkinIsVisible = true;
-                              }
-                            }
-                          }
-                          var whereObj = {name: "Off the Grid!", code: "XX", country: "Off the Grid!"};
-                          if (checkinIsVisible == true) {
-                            // Do database and display
-                            var lastCheckinLocation = lastCheckin.venue.location;
-                            var lastCheckinTS = lastCheckin.createdAt;
-                            var lastCheckinVenueId = lastCheckin.venue.id;
-                            var locationString = "";
-                            if (lastCheckinLocation.city !== undefined) {
-                                locationString = lastCheckinLocation.city;
-                            }
-                            if (lastCheckinLocation.country !== undefined) {
-                                if (locationString !== "") {
-                                    locationString = locationString + ", ";
-                                }
-                                locationString = locationString + lastCheckinLocation.country
-                            }
-                            whereObj = {name: lastCheckinLocation.country, code: lastCheckinLocation.cc, country: lastCheckinLocation.country};
-                            if (lastCheckinLocation.city !== undefined) whereObj.city = lastCheckinLocation.city;
-                            if (lastCheckinLocation.state !== undefined) whereObj.state = lastCheckinLocation.state;
-                            if (lastCheckinVenueId !== undefined) whereObj.foursquareid = lastCheckinVenueId;
-                            if (lastCheckinTS !== undefined) whereObj['lastseen_timestamp'] = lastCheckinTS;
-                            response["where"] = whereObj;
-                            // Store record
-                            dbparams.Item["where"] = whereObj;
-                            dynamo.putItem(dbparams, function(db) {
-                            });
-                            response["record"] = dbparams.Item["identifier"]
-                          }
-                          response.status = "OK";
-                          response.meta.status = 200;
+                    // The new way of doing things
+                    foursquare.checkinhistory({token: oauth_token}, function(cb) {
+                      response["status"] = cb["status"];
+                      if (cb["history"] != undefined) {
+                        var checkinHistory = cb["history"].reverse();
+                        var lastCheckin = checkinHistory[0];
+                        var checkinIsVisible = true;
+                        if (lastCheckin.visibility != undefined) {
+                          checkinIsVisible = false;
+                          console.log("Checkin not visible");
                         } else {
-                          response["status"] = respObj.meta.msg;
-                          response.meta.status = respObj.meta.code;
+                          console.log("Checkin is visible");
                         }
-                        response.meta.msg = response.status;
-                        context.succeed(response);
-                      }); // Finish processing
+                        if (checkinIsVisible == false) {
+                          for (var i = 1; i < checkinHistory.length; i++) {
+                            var checkinHistoryItem = checkinHistory[i];
+                            // If there is no visibility set for this checkin its good assuming we haven't already found a non off the grid checkin
+                            if (checkinHistoryItem.visibility == undefined && checkinIsVisible == false) {
+                              // Set lastCheckin to the next item thats available
+                              lastCheckin = checkinHistoryItem;
+                              checkinIsVisible = true;
+                            }
+                          }
+                        }
+                        var whereObj = {name: "Off the Grid!", code: "XX", country: "Off the Grid!"};
+                        if (checkinIsVisible == true) {
+                          // Do database and display
+                          var lastCheckinLocation = lastCheckin.venue.location;
+                          var lastCheckinTS = lastCheckin.createdAt;
+                          var lastCheckinVenueId = lastCheckin.venue.id;
+                          var locationString = "";
+                          if (lastCheckinLocation.city !== undefined) {
+                              locationString = lastCheckinLocation.city;
+                          }
+                          if (lastCheckinLocation.country !== undefined) {
+                              if (locationString !== "") {
+                                  locationString = locationString + ", ";
+                              }
+                              locationString = locationString + lastCheckinLocation.country
+                          }
+                          whereObj = {name: lastCheckinLocation.country, code: lastCheckinLocation.cc, country: lastCheckinLocation.country};
+                          if (lastCheckinLocation.city !== undefined) whereObj.city = lastCheckinLocation.city;
+                          if (lastCheckinLocation.state !== undefined) whereObj.state = lastCheckinLocation.state;
+                          if (lastCheckinVenueId !== undefined) whereObj.foursquareid = lastCheckinVenueId;
+                          if (lastCheckinTS !== undefined) whereObj['lastseen_timestamp'] = lastCheckinTS;
+                          response["where"] = whereObj;
+                          // Store record
+                          dbparams.Item["where"] = whereObj;
+                          dynamo.putItem(dbparams, function(db) {
+                          });
+                          response["record"] = dbparams.Item["identifier"]
+                        }
+                        response.status = "OK";
+                        response.meta.status = 200;
+                      } else {
+                        response.meta.status = 400;
+                      }
+                      context.succeed(response); // Return response
                     });
                   } else {
                     // Use cached entry
